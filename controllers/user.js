859244d07dda4ced99f73390705cf1c3159d154b
Brad Bailey <bbailey921@gmail.com>
2019-06-29 16:22:10 -0400

    add routes to user controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const userApi = require('../models/users.js')
-const userRouter = express.Router()
+const express = require('express'),
+  userApi = require('../models/users.js'),
+  userRouter = express.Router()
+
+userRouter.get('/new', (req, res) => {
+  res.render('user/newUser.hbs')
+})
+
+userRouter.get('/:_id', (req, res) => {
+  res.render('user/showUser.hbs')
+})
+
+userRouter.get('/:_id/edit', (req, res) => {
+  res.render('user/editUser.hbs')
+})
 
 module.exports = {
   userRouter

91aa7634893500237c8daa9243d679012bbe1623
Brad Bailey <bbailey921@gmail.com>
2019-07-01 10:10:03 -0400

    add routes to task controller file

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,6 +5,18 @@ TasksRouter.get('/', (req, res) => {
     res.send("Hello from Tasks page")
 })
 
+TasksRouter.get('/new', (req, res) => {
+    res.send("New Task Form")
+})
+
+TasksRouter.get('/:taskId', (req, res) => {
+    res.send("single task page")
+})
+
+TasksRouter.get('/:taskId/edit', (req, res) => {
+    res.send("task edit form")
+})
+
 module.exports = {
     TasksRouter
 };

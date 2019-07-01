e5a8e0643a8b4697bab3a72ef53b5a7aad55f9f2
Brad Bailey <bbailey921@gmail.com>
2019-07-01 11:30:39 -0400

    add delete route to projects controller

--- controllers/project.js
+++ controllers/project.js
@@ -7,11 +7,6 @@ ProjectRouter.get('/new', (req, res) => {
     res.render('projects/newProject.hbs')
 })
 
-ProjectRouter.get('/:userId', (req, res) => {
-    projectApi.getProjectsByUser(req.params.userId)
-        .then(res.send('Hello World'));
-})
-
 ProjectRouter.get('/:projectId', (req, res) => {
     projectApi.getProject(req.params.projectId)
         .then(project => {
@@ -23,6 +18,11 @@ ProjectRouter.get('/:projectId', (req, res) => {
 
 ProjectRouter.get('/:projectId/edit', (req, res) => {
     res.send('Project edit form');
+})
+
+ProjectRouter.delete('/:projectId/delete', (req, res) => {
+    projectApi.deleteProject(req.params.projectId)
+        .then(res.send('Project removed'))
 })
 
 module.exports = {

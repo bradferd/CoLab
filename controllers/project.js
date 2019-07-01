cfae1e85061bae2326465dd607e3b5b396b8a8fd
Brad Bailey <bbailey921@gmail.com>
2019-07-01 09:46:56 -0400

    add controllers for project

--- controllers/project.js
+++ controllers/project.js
@@ -2,6 +2,24 @@ const express = require('express'),
     projectApi = require('../models/projects'),
     projectRouter = express.Router();
 
+projectRouter.get('/:userId', (req, res) => {
+    projectApi.getProjectsByUser(req.params.userId)
+        .then(res.send('Hello World'));
+})
+
+projectRouter.get('/new', (req, res) => {
+    res.send('New Project Form')
+})
+
+projectRouter.get('/:projectId', (req, res) => {
+    projectApi.getProject(req.params.projectId)
+        .then(res.send('Hello from Project Page'));
+})
+
+projectRouter.get('/:projectId/edit', (req, res) => {
+    res.send('Project edit form');
+})
+
 module.exports = {
     projectRouter
 }

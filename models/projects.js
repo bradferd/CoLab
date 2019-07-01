93415342d168ea8c69923b3bccd72ca88b402103
Brad Bailey <bbailey921@gmail.com>
2019-07-01 08:50:49 -0400

    add models for projects

---
+++ models/projects.js
@@ -0,0 +1,27 @@
+const mongoose = require('./connection.js')
+
+const ProjectSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    userId: mongoose.Types.ObjectId
+})
+
+const ProjectCollection = mongoose.model('Projects', ProjectSchema)
+
+const getProjectsByUser = userId => ProjectCollection.find(userId)
+
+const getProject = projectId => ProjectCollection.findById(projectId)
+
+const newProject = projectObject => ProjectCollection.create(projectObject)
+
+const editProject = (projectId, projectObject) => ProjectCollection.findByIdAndUpdate(projectId, projectObject)
+
+const deleteProject = projectId => ProjectCollection.findByIdAndDelete(projectId)
+
+module.exports = {
+    getProjectsByUser,
+    getProject,
+    newProject,
+    editProject,
+    deleteProject
+}

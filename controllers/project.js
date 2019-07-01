cd928c964242535c13ffbd20da4117cf3d1e68fc
Brad Bailey <bbailey921@gmail.com>
2019-07-01 09:48:34 -0400

    reordered project controllers

--- controllers/project.js
+++ controllers/project.js
@@ -2,13 +2,13 @@ const express = require('express'),
     projectApi = require('../models/projects'),
     projectRouter = express.Router();
 
+projectRouter.get('/new', (req, res) => {
+    res.send('New Project Form')
+})
+
 projectRouter.get('/:userId', (req, res) => {
     projectApi.getProjectsByUser(req.params.userId)
         .then(res.send('Hello World'));
-})
-
-projectRouter.get('/new', (req, res) => {
-    res.send('New Project Form')
 })
 
 projectRouter.get('/:projectId', (req, res) => {

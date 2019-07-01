008b22f924a738ee8b5d7707bb01e0fc81f118ae
Brad Bailey <bbailey921@gmail.com>
2019-07-01 14:04:58 -0400

    add seed file

---
+++ seed.js
@@ -0,0 +1,60 @@
+const Users = require("./models/users");
+const Projects = require("./models/projects");
+const Tasks = require("./models/tasks");
+
+const data = [
+    {
+        name: "My Project 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Other Project 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Project 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+];
+
+function seedDB() {
+    Projects.remove({}, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("removed projects!");
+        Tasks.remove({}, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("removed tasks!");
+            //add a few campgrounds
+            data.forEach(function (seed) {
+                Projects.create(seed, function (err, projects) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("added a Project");
+                        //create a comment
+                        Tasks.create(
+                            {
+                                name: "Help me please",
+                                description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+                            }, function (err, comment) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("Created new task");
+                                }
+                            });
+                    }
+                });
+            });
+        });
+    });
+    //add a few comments
+}
+
+module.exports = seedDB;

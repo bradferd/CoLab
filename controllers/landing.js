3f1dded0248cff99588267ed22c16c7a521c8416
Brad Bailey <bbailey921@gmail.com>
2019-07-03 11:44:59 -0400

    add all projects route

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const projectsApi = require('../models/projects')
 const LandingRouter = express.Router();
 
 LandingRouter.get('/', (req, res) => {
     res.render("landing/home.hbs")
+})
+
+LandingRouter.get('/main', (req, res) => {
+    projectsApi.getAllProjects()
+        .then(projects => {
+            res.render('landing/allProjects', { projects })
+        })
 })
 
 module.exports = {
